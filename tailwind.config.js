d205bcdd4a5ce1cf1aae8d6471996c98e850178c
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-04-24 19:05:04 +0700

    [ADD] config web theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      purple: {
+        light: '#F4E5FF',
+        dark: '#7348D6',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#555555',
+        light: '#E5E5E5',
+      },
+      blue: {
+        light: '#D2F5F8',
+        DEFAULT: '#1AC6D4'
+      },
+      black: 'black',
+      white: {
+        dark: 'F9F9F9',
+        DEFAULT: 'FFFFFF'
+    },
+      green: 'green',
+      red: 'red'
+    }
   },
   variants: {
     extend: {},

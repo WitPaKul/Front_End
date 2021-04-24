420c1e3f8b3ed08c983371cd43746cb50227112c
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-04-24 19:00:35 +0700

    [ADD] fonts and router-link-active color

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
@@ -15,16 +13,21 @@
   color: #2c3e50;
 }
 
-#nav {
+@import 'https://fonts.googleapis.com/css?family=Mitr';
+h1,h2,h3{
+    font-family: 'Mitr', sans-serif;
+}
+
+/* #nav {
   padding: 30px;
 }
 
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-}
+} */
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #FFFFFF;
 }
 </style>

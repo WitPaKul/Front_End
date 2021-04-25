709f119415cbc8e73ebd962f09ba2058ad84b878
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-04-25 21:09:40 +0700

    [ADD] import component productItems

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,3 +1,14 @@
 <template>
   <navbar></navbar>
+  <product-items></product-items>
 </template>
+
+<script>
+import productItems from "../components/productItems.vue";
+
+export default {
+  components: {
+    productItems
+  },
+};
+</script>

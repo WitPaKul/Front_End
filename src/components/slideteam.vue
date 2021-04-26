dbefee6dfa9741861d78dd496f4da73b419d9173
Luck <pawat.rew@mail.kmutt.ac.th>
2021-04-26 18:54:46 +0700

    [Update] create slideteam and teammember

---
+++ src/components/slideteam.vue
@@ -0,0 +1,16 @@
+<template>
+        <img
+          class="object-cover object-center "
+          alt="hero"
+          src="https://dummyimage.com/1600x450"
+        />
+      
+</template>
+
+<script>
+export default {
+    name: 'slideteam',
+
+}
+</script>
+  

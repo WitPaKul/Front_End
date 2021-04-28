c51ebb0186792a5daf6fc11b709292c120c6568b
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-04-28 19:47:51 +0700

    [ADD] import component homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,15 @@
 <template>
   <navbar></navbar>
+  <homepage></homepage>
+  <footerbar></footerbar>
 </template>
+
+<script>
+import homepage from "../components/homepage.vue";
+
+export default {
+  components: {
+    homepage
+  },
+};
+</script>

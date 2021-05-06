0bb8e55f6e86fabae930416662b6a9807d2994e3
Luck <pawat.rew@mail.kmutt.ac.th>
2021-05-06 19:53:26 +0700

    [Update] Add Imaga connected backend

--- src/components/addproducts.vue
+++ src/components/addproducts.vue
@@ -1,87 +1,256 @@
 <template>
-
-<section class="text-gray-900 body-font overflow-hidden px-20"> 
-  <div class="h-12 md:items-center">
-    <div class="p-5 pl-20 text-purple-dark md:float-left flex">
-      <button type='button' @click="handleViewProducts" class="font-medium text-xl text-purple-dark border-transparent focus:outline-none">
-        View Products
-      </button>
-    <div class="px-7 text-gray-light font-bold">
-      |
-    </div>
-      <button type='button' @click="handleAddProduct" class="font-medium text-xl text-purple-dark border-transparent focus:outline-none">
-        Add Product
-      </button>
-    </div>
-  </div>
-  <div class="container px-5 py-24 mx-auto">
-    <div class="lg:w-4/5 mx-auto flex flex-wrap"> 
-      <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"  :src="image || 'https://dummyimage.com/800x800'">
-      <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <!--PRODUCT -->
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">PRODUCT NAME</h2>   
-            <h2 v-if="errors.indexOf('NoProductName') !== -1"  class="text-sm title-font text-red-600" >* Please Enter Product Name</h2>
-         <div class="flex mb-4">
-        <input v-model="collectdata.product_name" type="text" name="product-name" id="product-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm border border-gray-400 rounded-md">
-        </div>
-   <!-- Manufacturer Date -->
-  
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">Manufacturer Date</h2>   
-       <h2 v-if="errors.indexOf('NoDate') !== -1" class="text-sm title-font text-red-600 ">* Please Enter Date</h2>
-    
-      <div class="flex mb-4">
-      <input v-model="collectdata.product_manufactured_date" placeholder="dd-mm-yyyy" type="date" name="manufacturer-date" id="manufacturer-date" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm border border-gray-400 rounded-md">
+  <section class="text-gray-900 body-font overflow-hidden px-20">
+    <div class="h-12 md:items-center">
+      <div class="p-5 pl-20 text-purple-dark md:float-left flex">
+        <button
+          type="button"
+          @click="handleViewProducts"
+          class="font-medium text-xl text-purple-dark border-transparent focus:outline-none"
+        >
+          View Products
+        </button>
+        <div class="px-7 text-gray-light font-bold">|</div>
+        <button
+          type="button"
+          @click="handleAddProduct"
+          class="font-medium text-xl text-purple-dark border-transparent focus:outline-none"
+        >
+          Add Product
+        </button>
       </div>
-      <!-- Description -->
-    <div class="flex ">
-        <h2  class="text-sm title-font text-gray-500 tracking-widest">Description</h2>
-             <h2 v-if="errors.indexOf('Description') !== -1" class="text-sm title-font pl-4 text-red-600">* Please Enter Description</h2>         
-             </div>
-          <div class="flex mt-4 ">    
-            <textarea v-model="collectdata.product_description" class="w-full  focus:ring-indigo-500 focus:border-gray-900 block  h-32 text-base outline-none text-gray-700 py-1 px-3 shadow-sm border border-gray-400 rounded-md"></textarea>
-     </div>
-     
-       <!--Color  -->
-        <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+    </div>
+    <div class="container px-5 py-24 mx-auto">
+      <div class="lg:w-4/5 mx-auto flex flex-wrap">
+        <img
+          alt="ecommerce"
+          class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+          :src="image || 'https://dummyimage.com/800x800'"
+        />
+        <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+          <!--PRODUCT -->
+          <h2 class="text-sm title-font text-gray-500 tracking-widest">
+            PRODUCT NAME
+          </h2>
+          <h2
+            v-if="errors.indexOf('NoProductName') !== -1"
+            class="text-sm title-font text-red-600"
+          >
+            * Please Enter Product Name
+          </h2>
+          <div class="flex mb-4">
+            <input
+              v-model="collectdata.product_name"
+              type="text"
+              name="product-name"
+              id="product-name"
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm border border-gray-400 rounded-md"
+            />
+          </div>
+          <!-- Manufacturer Date -->
+
+          <h2 class="text-sm title-font text-gray-500 tracking-widest">
+            Manufacturer Date
+          </h2>
+          <h2
+            v-if="errors.indexOf('NoDate') !== -1"
+            class="text-sm title-font text-red-600"
+          >
+            * Please Enter Date
+          </h2>
+
+          <div class="flex mb-4">
+            <input
+              v-model="collectdata.product_manufactured_date"
+              placeholder="dd-mm-yyyy"
+              type="date"
+              name="manufacturer-date"
+              id="manufacturer-date"
+              class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm border border-gray-400 rounded-md"
+            />
+          </div>
+          <!-- Description -->
           <div class="flex">
-            <span class="mr-3">Color</span>
-            <button @click="handleColor(1)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 1}).length}" class="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(2)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 2}).length}" class="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(3)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 3}).length}" class="border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(4)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 4}).length}" class="border-2 border-gray-300 ml-1 bg-yellow-400 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(5)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 5}).length}" class="border-2 border-gray-300 ml-1 bg-purple-500 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(6)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 6}).length}" class="border-2 border-gray-300 ml-1 bg-white rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(7)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 7}).length}" class="border-2 border-gray-300 ml-1 bg-pink-500 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(8)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 8}).length}" class="border-2 border-gray-300 ml-1 bg-yellow-600 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button @click="handleColor(9)" :class="{'border-green-500': collectdata.product_colors.filter(product_color => {return product_color.color_id == 9}).length}" class="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>
+            <h2 class="text-sm title-font text-gray-500 tracking-widest">
+              Description
+            </h2>
+            <h2
+              v-if="errors.indexOf('Description') !== -1"
+              class="text-sm title-font pl-4 text-red-600"
+            >
+              * Please Enter Description
+            </h2>
+          </div>
+          <div class="flex mt-4">
+            <textarea
+              v-model="collectdata.product_description"
+              class="w-full focus:ring-indigo-500 focus:border-gray-900 block h-32 text-base outline-none text-gray-700 py-1 px-3 shadow-sm border border-gray-400 rounded-md"
+            ></textarea>
+          </div>
+
+          <!--Color  -->
+          <div
+            class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"
+          >
+            <div class="flex">
+              <span class="mr-3">Color</span>
+              <button
+                @click="handleColor(1)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 1;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(2)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 2;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(3)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 3;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(4)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 4;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-yellow-400 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(5)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 5;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-purple-500 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(6)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 6;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-white rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(7)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 7;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-pink-500 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(8)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 8;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-yellow-600 rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+              <button
+                @click="handleColor(9)"
+                :class="{
+                  'border-green-500': collectdata.product_colors.filter(
+                    (product_color) => {
+                      return product_color.color_id == 9;
+                    }
+                  ).length,
+                }"
+                class="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"
+              ></button>
+            </div>
+          </div>
+          <!-- Price -->
+          <div class="flex">
+            <h2 class="text-sm title-font text-gray-500 tracking-widest">
+              Price
+            </h2>
+            <input
+              v-model="collectdata.product_price"
+              type="text"
+              name="product-price"
+              id="product-price"
+              class="ml-4 shadow-sm border border-gray-400 rounded-md"
+            />
+            <h2
+              v-if="errors.indexOf('Noprice') !== -1"
+              class="text-sm title-font text-red-600 pl-4"
+            >
+              * Please Enter Price
+            </h2>
+          </div>
+          <!-- Photo -->
+          <div class="flex mt-6">
+            <input
+              type="file"
+              id="imageFile"
+              @change="uploadImage($event)"
+              multiple
+              accept=".jpg, .jpeg, .png"
+            />
+          </div>
+          <h2
+            v-if="errors.indexOf('img') !== -1"
+            class="text-sm title-font text-red-600 pr-32"
+          >
+            * Please Select Photo
+          </h2>
+          <!--ADD  Cancel-->
+          <div class="flex mt-6">
+            <button
+              class="flex ml-auto text-white bg-blue border-0 py-2 px-6 focus:outline-none hover:bg-blue-500 rounded"
+              @click="AddForm"
+            >
+              Add
+            </button>
+            <button
+              class="flex-initial text-white ml-4 bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
+            >
+              Cancel
+            </button>
           </div>
         </div>
-        <!-- Price -->
-        <div class="flex ">          
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">Price</h2> 
-        <input v-model="collectdata.product_price" type="text" name="product-price" id="product-price" class="ml-4  shadow-sm border border-gray-400 rounded-md">
-      <h2 v-if="errors.indexOf('Noprice') !== -1" class="text-sm title-font text-red-600 pl-4 ">* Please Enter Price</h2>         
-     </div>
-     <!-- Photo -->
-      <div class="flex mt-6">
-        <input type="file" id="imageFile"  @change="uploadImage($event)" multiple accept=".jpg, .jpeg, .png" >  
-     </div>
-      <h2 v-if="errors.indexOf('img') !== -1"
-          class="text-sm title-font text-red-600 pr-32">* Please Select Photo</h2>
-          <!--ADD  Cancel-->
-      <div class="flex mt-6">
-          <button class="flex ml-auto text-white  bg-blue border-0 py-2 px-6 focus:outline-none hover:bg-blue-500 rounded" @click="AddForm" >Add</button>
-        <button class=" flex-initial text-white ml-4  bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Cancel</button>
-        </div>
       </div>
     </div>
-  </div>
-     
-
-</section>
+  </section>
 </template>
 
 <script>
+const axios = require("axios");
 const baseURL = "http://localhost:5000"
 
 export default {
@@ -101,6 +270,7 @@ data() {
               product_colors: [],
               product_brand: null
           },
+          product_code: 0
         }
     },
 
@@ -118,20 +288,46 @@ data() {
                 reader.readAsDataURL(input.files[0])
             }        
         },  
-    AddForm() {           
+    async AddForm() {           
             // this.errordata();
             // if (this.errors.length > 0) {
             //     return;
             // }
             this.collectdata.product_code = null
             this.collectdata.product_brand = {'brand_id': 1}
-            fetch(baseURL+"/add_product", {
+            await fetch(baseURL+"/add_product", {
                 method: "POST",
                 headers: {
                     "content-type": "application/json",
                 },
                 body: JSON.stringify(this.collectdata),
-            });              
+            }).then(response => {
+                return response.json()
+            })
+            .then(data => {
+                // console.log(data);
+                // console.log(this.collectdata.product_image);
+                const fileInput = document.querySelector('#imageFile') ;
+                const formData  = new FormData();
+
+                formData.append("file", fileInput.files[0]);
+                axios.post("http://localhost:5000/image/add/" + data + "_" + this.collectdata.product_image, formData)
+            })
+
+            // fetch(baseURL + "/image/add/2", {
+            //   method: 'POST',
+            //   headers: {'Content-Type':'multipart/form-data'},
+            //   body: formData,
+            //   mode: 'no-cors'
+            // });
+            
+            // fetch(baseURL + "/image/add/2", {        
+            //    method: 'POST',         
+            //    headers: {'Content-Type':'multipart/form-data'},        
+            //     body: new FormData(document.getElementById('imageFile'))     })         
+            //     .then((response) => response.json());
+    
+                 
     },
 
     errordata() {          
@@ -169,7 +365,10 @@ data() {
 
 <style scoped>
 @import "https://fonts.googleapis.com/css?family=Mitr";
-h1,h2,h3,section {
+h1,
+h2,
+h3,
+section {
   font-family: "Mitr", sans-serif;
 }
 </style>

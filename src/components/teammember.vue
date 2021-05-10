387f172b274cce879d605eb387d72c930f37c6c9
Luck <pawat.rew@mail.kmutt.ac.th>
2021-05-11 02:17:38 +0700

    [ADD] add pictures for every page to frontend

--- src/components/teammember.vue
+++ src/components/teammember.vue
@@ -4,54 +4,42 @@
       <div class="flex flex-wrap  justify-center">
         <div class="p-4 lg:w-1/4 md:w-1/2">
           <div class="h-full flex flex-col items-center text-center">
-            <img
-              alt="team"
-              class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-              src="https://dummyimage.com/200x200"
-            >
-            <div class="w-full">
+              <img  src="@/assets/Team/074REw.jpg" >
+            <div class="pt-4 w-full">
               <h2 class="title-font sm:text-2xl text-2xl mb-4 font-medium text-purple-dark">
               62130500074
               </h2>
               <h3 class="title-font sm:text-xl text-xl mb-4 font-medium text-purple-900">Pawat Munkong</h3>
               <p class="title-font sm:text-xl text-xl  font-medium mb-4 text-purple-800">
-                details : เทพ
+                Roles : Front-end , Back-end
               </p>
             </div>
           </div>
         </div>
-        <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class=" p-4 lg:w-1/4 md:w-1/2">
           <div class="h-full flex flex-col items-center text-center">
-            <img
-              alt="team"
-              class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-              src="https://dummyimage.com/201x201"
-            >
-            <div class="w-full">
+             <img  src="@/assets/Team/082Earty.jpg" >
+            <div class="pt-4 w-full">
               <h2 class="title-font sm:text-2xl text-2xl mb-4 font-medium text-purple-dark">
               62130500082
               </h2>
               <h3 class="title-font sm:text-xl text-xl mb-4 font-medium text-purple-900">Worawit Makkasan</h3>
               <p class="title-font sm:text-xl text-xl mb-4 font-medium text-purple-800">
-               details : กุ๊กๆๆๆ
+               Roles : Back-end , Database  DevOps 
               </p>
             </div>
           </div>
         </div>
         <div class="p-4 lg:w-1/4 md:w-1/2">
           <div class="h-full flex flex-col items-center text-center">
-            <img
-              alt="team"
-              class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-              src="https://dummyimage.com/202x202"
-            >
-            <div class="w-full">
+           <img  src="@/assets/Team/090.jpg" >
+            <div class="pt-4 w-full">
               <h2 class="title-font sm:text-2xl text-2xl mb-4 font-medium text-purple-dark">
                 62130500090
               </h2>
               <h3 class="title-font sm:text-xl text-xl mb-4 font-medium text-purple-900">Sakulthip Rassameecharoentham</h3>
               <p class="title-font sm:text-xl text-xl mb-4 font-medium text-purple-800">
-              details : หยกกกกกก
+              Roles : Front-end , Back-end  Database
               </p>
             </div>
           </div>

648d02ec5609af7882e22dd8c845154466670454
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-05-10 23:05:58 +0700

    [ADD] make navbar responsive

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,49 +1,64 @@
-<template>
-<div class="bg-purple-light">      
-        <!-- Box alignment -->
-        <nav class="flex items-center h-20">
-            <div>
-                <router-link to="/">
-                    <!-- <img src="" class="header-logo max-h-14" title="Witpakul Shop Official"> -->
-                        <p class="font-semibold text-xl text-black px-28">Witpakul Shop</p>
-                </router-link>
+<template>   
+<div class="bg-purple-light">
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        
+                <div class="flex items-center flex-shrink-0 text-white mr-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 rounded-full" viewBox="0 0 24 24">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                    </svg>
+                    <router-link to="/">
+                        <p class="font-semibold text-xl text-black">Witpakul Shop</p>
+                    </router-link>
+                </div>
+        <div class="block lg:hidden">
+            <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" @click="handle_collapse">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            </button>
+        </div>
+        <div :class="{'block': showFullNavbarFlag, 'hidden': !showFullNavbarFlag}" class="w-full flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex lg:justify-center lg:flex-grow">
+                <div class="flex items-center flex-shrink-0">
+                    <router-link to="/about" class="menu-option">
+                        <button class="font-medium text-xl text-purple-dark block my-2 lg:inline-block lg:my-0 text-teal-200 hover:text-white mx-4">
+                            <p>About</p>
+                        </button>
+                    </router-link> 
+                </div>
+                <div class="flex items-center flex-shrink-0">
+                    <router-link to="/products" class="menu-option">
+                        <button class="font-medium text-xl text-purple-dark block my-2 lg:inline-block lg:my-0 text-teal-200 hover:text-white mx-4">
+                            <p>Products</p>
+                        </button>
+                    </router-link> 
+                </div>
+                <div class="flex items-center flex-shrink-0">
+                    <router-link to="/team" class="menu-option">
+                        <button class="font-medium text-xl text-purple-dark block my-2 lg:inline-block lg:my-0 text-teal-200 hover:text-white mx-4">
+                            <p>Team</p>
+                        </button>
+                    </router-link>
+                </div>
             </div>
-            <div>
-                <ul class="flex flex-row pl-10 place-self-center">
-                    <!-- Spacing -->
-                    <!-- Typography-->
-                    <li class="pr-4">
-                        <button class="font-medium text-xl text-purple-dark">
-                            <router-link to="/about">
-                                 <p>About</p>
-                            </router-link> 
-                        </button>
-                    </li>
-                    <li class="pr-4">
-                        <button class="font-medium text-xl text-purple-dark">
-                            <router-link to="/products">
-                                <p>Products</p>
-                            </router-link> 
-                        </button>
-                    </li>
-                    <li class="pr-4">
-                        <button class="font-medium text-xl text-purple-dark">
-                            <router-link to="/team">
-                                <p>Team</p>
-                            </router-link>
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>     
+        </div>
+    </nav>
+</div>
 </template>
 
 <script>
 export default {
     name: 'navbar',
-    props:{
+    props: {
         msg: String
+    },
+    data() {
+        return {
+            showFullNavbarFlag: true
+        };
+    },
+    methods: {
+        handle_collapse() {
+            this.showFullNavbarFlag = !this.showFullNavbarFlag;
+        }
     }
 }
 </script>
@@ -53,8 +68,15 @@ export default {
 h1,h2,h3, p{
     font-family: 'Mitr', sans-serif;
 }
+.menu-option {
+    color: #FFFFFF;
+    border-radius: 25px;
+    border: 4px solid #00000000;
+}
 .router-link-active {
     color: #FFFFFF;
+    border-radius: 25px;
+    border: 4px solid #1AC6D4;
 }
 
 </style>
